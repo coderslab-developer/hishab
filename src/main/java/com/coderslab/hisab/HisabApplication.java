515768ea926affc2843691d496d3cb3963566fd2
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2018-05-21 03:51:51 +0600

    21-05-2018 first commit

---
+++ src/main/java/com/coderslab/hisab/HisabApplication.java
@@ -0,0 +1,12 @@
+package com.coderslab.hisab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisabApplication.class, args);
+	}
+}
---
+++ src/test/java/com/coderslab/hisab/HisabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coderslab.hisab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HisabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
